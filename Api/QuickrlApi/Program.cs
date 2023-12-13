18e454799356a68eb747b2901514655bb8b13f85
jordan.woodruff <jordan.woodruff@tylertech.com>
2023-12-13 12:47:28 -0600

    Upgrade to dotnet 6 & cleanup

--- Api/QuickrlApi/Program.cs
+++ Api/QuickrlApi/Program.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace QuickrlApi
 {
@@ -20,6 +14,7 @@ namespace QuickrlApi
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls(("http://localhost:5004"));
                     webBuilder.UseStartup<Startup>();
                 });
     }
